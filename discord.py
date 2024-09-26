56cd91fa585fd685697dfd50767d62940b75b120
itojun-1230 <juntou1230@gmail.com>
2024-09-26 22:48:59 +0900

    add: Discord Botからメッセージの送信を実行

--- discord.py
+++ discord.py
@@ -1,1 +1,18 @@
-print("Hello Python!")
+import sys
+import requests
+
+args = sys.argv
+botToken = args[1]
+channelId = args[2]
+
+url = f"https://discord.com/api/v10/channels/{channelId}/messages"
+headers = {
+    "Authorization": f"Bot {botToken}",
+    "Content-Type": "application/json",
+}
+payload = {
+    "content": "Hello, World!",
+}
+r = requests.post(url, headers=headers, json=payload)
+
+print(r.text)
