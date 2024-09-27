3ad8946c02771794c0e60537b94ff9e9c9543bf3
itojun-1230 <juntou1230@gmail.com>
2024-09-27 12:44:44 +0900

    add: GASにメッセージIDを保存

--- discord.py
+++ discord.py
@@ -4,14 +4,21 @@ import datetime
 import time
 import requests
 
-def main():
-    args = sys.argv
-    botToken = args[1]
-    channelId = args[2]
+args = sys.argv
+botToken = args[1]
+channelId = args[2]
+gasURL = f"https://script.google.com/macros/s/AKfycbwM8MkxtgbiBhKnlge2HmV1TodCc6woucUJ_wJXaiLcG-YNsgEt6NtYhYkcaH3q_xl1/exec"
 
-    messageId = askAttendance(botToken, channelId)
+
+def main():
+
+    messageResult = askAttendance(botToken, channelId)
+    messageId = messageResult[0]
+    title = messageResult[1]
     createReaction(botToken, channelId, messageId, "⭕")
     createReaction(botToken, channelId, messageId, "❌")
+
+    postMessageId(messageId, title)
 
 def askAttendance(botToken, channelId):
     url = f"https://discord.com/api/v10/channels/{channelId}/messages"
@@ -20,10 +27,11 @@ def askAttendance(botToken, channelId):
         "Content-Type": "application/json",
     }
     ruleId = 1288860377634963558
+    title = f"{get_time()}の活動の参加可否"
     payload = {
         "embeds": [
             {
-                "title": f"{get_time()}の活動の参加可否",
+                "title": title,
                 "description": (
                     f"<@&{ruleId}>\n"
                     "お疲れ様です！\n"
@@ -38,7 +46,7 @@ def askAttendance(botToken, channelId):
     }
     r = requests.post(url, headers=headers, json=payload)
     print(r.json())
-    return r.json()["id"]
+    return r.json()["id"], r.json()["embeds"][0]["title"]
 
 def createReaction(botToken, channelId, messageId, reaction):
     url = f"https://discord.com/api/v10/channels/{channelId}/messages/{messageId}/reactions/{reaction}/@me"
@@ -48,8 +56,9 @@ def createReaction(botToken, channelId, messageId, reaction):
     }
     requests.put(url, headers=headers)
     time.sleep(1)
-    
 
+def postMessageId(messageId, title):
+    requests.post(gasURL, json={"messageId": messageId, "title": title})
 
 def get_time():
     nextDay = datetime.datetime.now() + datetime.timedelta(days=1)
